3f20ed14c8379dd24927ca38d2248c9419ac0b47
kaye2512 <kayeromy@gmail.com>
2025-04-29 21:16:54 +0200

    Update dependencies and add cursor rules to .gitignore; enhance global styles with Tailwind CSS and tw-animate-css integration.

---
+++ app/signin/page.tsx
@@ -0,0 +1,9 @@
+import SignInForm from "./signin-form";
+
+export default function SignIn() {
+  return (
+    <div className="w-full h-screen flex justify-center items-center">
+      <SignInForm />
+    </div>
+  );
+}
---
+++ app/signup/page.tsx
@@ -0,0 +1,9 @@
+import SignUpForm from "./signup-form";
+
+export default function SignUp() {
+  return (
+    <div className="w-full h-screen flex justify-center items-center">
+      <SignUpForm />
+    </div>
+  );
+}
